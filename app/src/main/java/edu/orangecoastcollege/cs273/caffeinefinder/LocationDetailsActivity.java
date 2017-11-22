a0f25655957d65cb427f44258b5070cfc829754c
brendanAlbert <alber123@mail.chapman.edu>
2017-11-21 17:39:25 -0800

    added LocationDetailsActivity

--- app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/LocationDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/LocationDetailsActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class LocationDetailsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class LocationDetailsActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private Location selectedLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,13 +17,15 @@ public class LocationDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location_details);
 
         // Map Fragment
+
+
         TextView locationNameTextView = (TextView) findViewById(R.id.locationNameTextView);
         TextView locationAddressTextView = (TextView) findViewById(R.id.locationAddressTextView);
         TextView locationCityStateZipTextView = (TextView) findViewById(R.id.locationCityStateZipTextView);
         TextView locationPhoneTextView = (TextView) findViewById(R.id.locationPhoneTextView);
         TextView locationLatLngTextView = (TextView) findViewById(R.id.locationLatLngTextView);
 
-        Location selectedLocation = getIntent().getExtras().getParcelable("SelectedLocation");
+        selectedLocation = getIntent().getExtras().getParcelable("SelectedLocation");
 
         locationNameTextView.setText(selectedLocation.getName());
         locationAddressTextView.setText(selectedLocation.getAddress());
@@ -34,6 +41,11 @@ public class LocationDetailsActivity extends AppCompatActivity {
         String latitudeTextString = String.valueOf(selectedLocation.getLatitude() + "W");
 
         locationLatLngTextView.setText(longitudeTextString + " " + latitudeTextString);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
